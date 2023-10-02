625ea06f02aadfee61602c9b214c1e7085c8cf9f
DRLOV <phukondurlov45@gmail.com>
2023-10-02 16:18:25 +0530

    sucessfully create react app

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,12 +1,9 @@
 
-
 function App() {
-
 
   return (
     <>
-      <h1>Namste Duniya</h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi, itaque mollitia optio commodi adipisci rem blanditiis numquam tenetur sapiente nulla explicabo dolor minima? Architecto magnam odio aperiam, blanditiis recusandae beatae, libero adipisci perferendis quisquam reiciendis maiores nostrum tenetur qui non! Quisquam cupiditate quidem tempore blanditiis maiores qui, ad officia id?</p>
+      <h1>Chai aur react || DRLOV</h1>
     </>
   )
 }
