f77b4f8591f9f46f4994cf9fff826e1ddfbbf0e2
DRLOV <phukondurlov45@gmail.com>
2023-10-06 10:53:41 +0530

    react-hooks complete

---
+++ 02_counter_project/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(10);
+  // let counter = 10;
+
+  const addValue = () => {
+    setCounter(counter + 1);
+    if (counter === 20) {
+      setCounter(0);
+    }
+  }
+  const removeValue = () => {
+    setCounter(counter - 1);
+    if (counter === 0) {
+      setCounter(0);
+    }
+  }
+
+  return (
+    <>
+      <h1>Chai Aur React || DRLOV</h1>
+      <h2>counter: {counter}</h2>
+      <h2>counter1: {counter}</h2>
+      <h2>counter2: {counter}</h2>
+      <h2>counter3: {counter}</h2>
+      <h2>counter4: {counter}</h2>
+      <button onClick={addValue}>add value</button>
+      <button onClick={removeValue}>remove value</button>
+    </>
+  )
+}
+
+export default App
